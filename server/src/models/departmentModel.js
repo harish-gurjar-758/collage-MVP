1271a9250d70d0adf78f9583bd9f222338a2c21a
harish-gurjar-758 <gurjarharish758@gmail.com>
2026-02-17 15:54:34 +0530

    .

--- server/src/models/departmentModel.js
+++ server/src/models/departmentModel.js
@@ -1,10 +1,13 @@
 import mongoose from "mongoose";
 
+const subjectSchema = mongoose.Schema({
+subjectName:{type:String},
+
+})
+
 const departmentSchema = mongoose.Schema({
     departName: { type: String, required: true },
-    subjects: [
-        {}
-    ]
+    subjects: {}
 });
 
 const Department = mongoose("Departments", departmentSchema);
